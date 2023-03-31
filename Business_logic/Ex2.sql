2a065700e6f329709dc343683f93bce07c09d5a1
Amadeusz <125867556+AmadeuszRaczynski@users.noreply.github.com>
2023-03-31 10:47:50 +0200

    Update Ex2.sql

--- Business_logic/Ex2.sql
+++ Business_logic/Ex2.sql
@@ -1,16 +1,15 @@
 
-
 /*Mechanism of automatically marking orders as inactive in the absence of accepted payment within 
 8 days of the order.*/
 
-
+-- PACKAGE 
 CREATE OR REPLACE PACKAGE validation_pkg
 IS
 PROCEDURE p_validator;
 END validation_pkg;
 /
 
-
+-- PACKAGE BODY
 
 CREATE OR REPLACE PACKAGE BODY validation_pkg
 IS
@@ -42,7 +41,7 @@ END validation_pkg;
 
 
 
-
+--JOB program
 BEGIN
 DBMS_SCHEDULER.create_program (
 program_name     => 'automat_validate_orders' ,
@@ -54,7 +53,7 @@ comments         => 'Order validator'
 END;
 /
 
-
+-- JOB schedule
 BEGIN
 DBMS_SCHEDULER.create_schedule (
 schedule_name    => 'Weekly_harmonogram',
@@ -62,6 +61,19 @@ start_date       => SYSTIMESTAMP,
 repeat_interval  => 'FREQ=DAILY; BYDAY=THU; BYHOUR = 17; BYMINUTE=24;',
 end_date         => NULL,
 comments         => 'Repeated every week on Monday'
+);
+END;
+/
+
+
+-- JOB 
+BEGIN
+DBMS_SCHEDULER.create_job (
+job_name        =>  'orders_validator',
+program_name    =>  'automat_validate_orders',
+schedule_name   =>  'Weekly_harmonogram',
+enabled         =>  TRUE,
+comments        =>  'TEST JOB'
 );
 END;
 /
